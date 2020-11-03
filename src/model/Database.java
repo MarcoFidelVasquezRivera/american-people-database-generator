e6740d724e26ea8f2c4fa643086b6667fd5565b3
Marco <macroxx120@gmail.com>
2020-11-02 21:07:51 -0500

    created Database class which will be the controller

---
+++ src/model/Database.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+
+public class Database {
+	/*	a randomly generated code
+	 * 	a name 
+	 * 	last name
+	 * 	gender
+	 *  birthdate
+	 *  height
+	 *  nationality 
+	 *  and a picture and save it to the database.
+	 */
+
+	public void generatePeople() {
+		
+	}
+	
+	public int generateCode() {
+		Random random = new Random();
+		return 0;
+	}
+	
+	public int generateAge() {
+		return 0;
+	}
+	
+	public String generateNationality() {
+		return "";
+	}
+	
+	
+}
